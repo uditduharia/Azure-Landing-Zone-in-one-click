8733263793777148bbd74646168e4fcedebd36dd
uditduharia <35994008+uditduharia@users.noreply.github.com>
2026-09-20 10:08:30 +0530

    Upgrade azurerm provider and change backend settings
    Updated Azure provider version and backend configuration.

--- environments/preprod/provider.tf
+++ environments/preprod/provider.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.80.0"
+      version = "5.0.0"
     }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-prod"
-    storage_account_name = "ppsstorageee"
-    container_name       = "tfstate"
-    key                  = "thisterraform.tfstate"
+    resource_group_name  = "restart"
+    storage_account_name = "ppssttoorraaggee"
+    container_name       = "ppscontainer"
+    key                  = "testterra.tfstate"
   }
 
 }
